b95a9fc8a1e4908883685b9bf1849eca7171bbf7
Daqing Feng <fengdaqing@hotmail.com>
2018-06-26 15:25:22 +0800

    add the spring boot admin webui

--- techprimers-admin/src/main/java/com/techprimers/admin/AdminApplication.java
+++ techprimers-admin/src/main/java/com/techprimers/admin/AdminApplication.java
@@ -1,9 +1,11 @@
 package com.techprimers.admin;
 
+import de.codecentric.boot.admin.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableAdminServer
 public class AdminApplication {
 
 	public static void main(String[] args) {
